e20647d72292540d81c8be7b296c9dde5d48cc2e
ABYUF <abrarbasha.clouddevops@gmail.com>
2025-08-20 18:01:28 +0530

    Updated space gap in test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -4,5 +4,5 @@ def test_index():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b" Hello, Flask, Gunicorn, Nginx is successfully working, Welcome to CI/CD Pipeline, DevOps!" in response.data
+    assert b"Hello, Flask, Gunicorn, Nginx is successfully working, Welcome to CI/CD Pipeline, DevOps!" in response.data
 
