bef0aaa97d1962474fe55625f6c11b8cb5f4cb38
ABYUF <abrarbasha.clouddevops@gmail.com>
2025-08-20 17:47:09 +0530

    Updated Flask Message

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask is working, Welcome to CI/CD Pipeline, DevOps!"
+    return "Hello, Flask, Gunicorn, Nginx is successfully working, Welcome to CI/CD Pipeline, DevOps!"
 
 if __name__ == '__main__':
     app.run()
